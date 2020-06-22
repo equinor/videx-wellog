837fbf232d1b910ec3d7c2bdbd2fce10d34f6ac8
Brynjulf <brisb@equinor.com>
2020-06-22 14:38:19 +0200

    added dual scale example with log viewer

--- examples/plain-javascript-example/src/log.viewer/log.viewer.stories.js
+++ examples/plain-javascript-example/src/log.viewer/log.viewer.stories.js
@@ -2,6 +2,7 @@ export default { title: 'Log Viewer' };
 
 export * from './log.viewer.story';
 export * from './log.viewer.multiple.tracks.story';
+export * from './log.viewer.dual.scale.track.story';
 export * from './log.viewer.legend.story';
 export * from './log.viewer.horizontal.story';
 export * from './wellog.component.story';
