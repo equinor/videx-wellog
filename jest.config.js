229e702baf18c8ef28e045056afb628fe1c8b5b5
tomktjemsland <tom.tjemsland@bouvet.no>
2023-12-06 19:01:28 +0100

    Updated moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -8,8 +8,8 @@ module.exports = {
   ],
   testEnvironment: 'node',
   moduleNameMapper: {
-    "^d3-drag": "<rootDir>/node_modules/d3-zoom/node_modules/d3-drag/dist/d3-drag.min.js",
-    "^d3-transition": "<rootDir>/node_modules/d3-zoom/node_modules/d3-transition/dist/d3-transition.min.js",
+    "^d3-drag": "<rootDir>/node_modules/d3-drag/dist/d3-drag.min.js",
+    "^d3-transition": "<rootDir>/node_modules/d3-transition/dist/d3-transition.min.js",
     "^d3-(.*)$": "<rootDir>/node_modules/d3-$1/dist/d3-$1.min.js",
   }
 };
