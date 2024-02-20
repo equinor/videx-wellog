217c1da1478b956f5f58e2e07cf1575621672f74
Fabien Bosquet <fabien.bosquet@pdgm.com>
2024-02-20 17:09:21 +0100

    delete old storybook based on version 5

--- examples/plain-javascript-example/src/plot/plot.stories.js
+++
@@ -1,6 +0,0 @@
-export default { title: 'Plot types' };
-
-export * from './line.plot.story';
-export * from './area.plot.story';
-export * from './dot.plot.story';
-export * from './differential.plot.story';
--- examples/plain-javascript-example/src/track/track.stories.js
+++
@@ -1,9 +0,0 @@
-
-export default { title: 'Track types' };
-
-export * from './scale.track.story';
-export * from './scale.track.dual.story';
-export * from './graph.track.basic.story';
-export * from './graph.track.single.plot.story';
-export * from './graph.track.multiple.plots.story';
-export * from './stacked.track.story';
