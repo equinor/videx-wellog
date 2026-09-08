2b9f3fa34be51a9f7150a67e11a0aed7851968b4
Svein Erik Lundervik Fornes <41449429+sefornes@users.noreply.github.com>
2026-09-08 10:47:15 +0200

    ci: add workflow for code coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,14 @@ export default defineConfig({
     globals: true,
     // Environment
     environment: 'jsdom',
+    reporters: ['default', 'junit'],
+    outputFile: {
+      junit: 'test-results/junit.xml',
+    },
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', ['cobertura', { file: 'Cobertura.xml' }]],
+      reportsDirectory: 'coverage',
+    },
   },
 });
